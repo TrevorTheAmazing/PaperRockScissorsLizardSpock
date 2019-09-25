eab5904491b082aabb208dea69d6a022f7d358f4
trevor clements <trevortheamazing@gmail.com>
2019-09-25 14:05:44 -0500

    classes progress

---
+++ ClassFiles/Gesture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperRockScissorsLizardSpock.ClassFiles
+{
+    class Gesture
+    {
+        public string[] GestureArray = ["Paper", "Rock", "Scissors", "Lizard", "Spock"];
+
+        public Gesture()
+        {
+            this.GestureArray = GestureArray;
+
+
+        }
+    }
+
+}
